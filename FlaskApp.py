a9c3a2509c3a1ff7c0585ea0f0b093a618ab607d
zwx419685574 <419685574@qq.com>
2018-06-30 15:58:32 +0800

    conn db

--- FlaskApp.py
+++
@@ -1,8 +0,0 @@
-# -*- encoding: utf-8 -*-
-
-
-from app import app
-
-
-if __name__ == '__main__':
-    app.run()
--- app/__init__.py
+++
@@ -1,7 +0,0 @@
-# coding:utf-8
-
-from flask import Flask
-
-app = Flask(__name__)
-
-from app import views
